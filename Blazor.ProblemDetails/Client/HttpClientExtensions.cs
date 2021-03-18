d3a1733b63c701a46617c86c1fd23321f065ea11
jimmys20 <maragkosd12@gmail.com>
2021-03-18 20:58:26 +0200

    ProblemDetails DelegatingHandler

--- Blazor.ProblemDetails/Client/HttpClientExtensions.cs
+++ Blazor.ProblemDetails/Client/HttpClientExtensions.cs
@@ -8,21 +8,21 @@ namespace Blazor.ProblemDetails.Client
 {
     public static class HttpClientExtensions
     {
-        public static Task<TValue> GetJsonAsync<TValue>(this HttpClient client, string requestUri, CancellationToken cancellationToken = default)
-            => client.GetJsonAsync<TValue>(requestUri, options: null, cancellationToken);
+        //public static Task<TValue> GetJsonAsync<TValue>(this HttpClient client, string requestUri, CancellationToken cancellationToken = default)
+        //    => client.GetJsonAsync<TValue>(requestUri, options: null, cancellationToken);
 
-        public static async Task<TValue> GetJsonAsync<TValue>(this HttpClient client, string requestUri, JsonSerializerOptions options, CancellationToken cancellationToken = default)
-        {
-            using var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+        //public static async Task<TValue> GetJsonAsync<TValue>(this HttpClient client, string requestUri, JsonSerializerOptions options, CancellationToken cancellationToken = default)
+        //{
+        //    using var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
 
-            if (response.IsSuccessStatusCode)
-            {
-                return await response.Content.ReadFromJsonAsync<TValue>(options, cancellationToken).ConfigureAwait(false);
-            }
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        return await response.Content.ReadFromJsonAsync<TValue>(options, cancellationToken).ConfigureAwait(false);
+        //    }
 
-            var problem = await response.Content.ReadFromJsonAsync<ProblemDetails>(options, cancellationToken).ConfigureAwait(false);
+        //    var problem = await response.Content.ReadFromJsonAsync<ProblemDetails>(options, cancellationToken).ConfigureAwait(false);
 
-            throw new ProblemDetailsException(problem);
-        }
+        //    throw new ProblemDetailsException(problem);
+        //}
     }
 }
